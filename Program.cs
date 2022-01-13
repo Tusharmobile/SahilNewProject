86e6128d9ea8df4d558c2ce6ad4cc052f3be9b83
tushar <tusharshitre123@gmail.com>
2022-01-13 18:45:02 +0530

    This is new repo create from me for nick name

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SahilNewProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the new repo from tushar");
+            Console.WriteLine("Dont should be laghing");
+            Console.ReadLine();
+        }
+    }
+}
